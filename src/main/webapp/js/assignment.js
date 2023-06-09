921b9daf03553be105ca2bfcacb9e09b1992fa20
bnbu <jsh031613@naver.com>
2023-06-09 16:29:57 +0900

    init

---
+++ src/main/webapp/js/assignment.js
@@ -0,0 +1,21 @@
+new Swiper('header .swiper-container', {
+    autoplay: true,
+    loop: true,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: 'header .swiper-prev',
+        nextEl: 'header .swiper-next'
+    }
+})
+
+const imgs = document.querySelectorAll("img");
+const mainImg = document.querySelector(".main-image img");
+
+imgs.forEach((e) => {
+    e.addEventListener("click", (t) => {
+        let src = t.target.getAttribute("src");
+        console.log(src)
+        mainImg.setAttribute("src", src);
+    });
+});
+
